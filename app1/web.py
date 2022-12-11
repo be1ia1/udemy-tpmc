119f02c9634ba5b3ae0c9437897e18cf0396c29e
Andriy Gula <gula@pr.km.ua>
2022-12-11 23:50:47 +0200

    fix web.py

--- app1/web.py
+++ app1/web.py
@@ -1,29 +1,26 @@
 import streamlit as st
 import functions
 
-
+if 'new_todo' not in st.session_state:
+   st.session_state.new_todo = ''
 
 todos = functions.get_todos()
 
-def add_todo():
-
-    todo = st.session_state['new_todo']
-    #st.write(st.session_state.new_todo)
-    todos.append(todo)
-    functions.write_todos(todos)
+def update_new_todo():
+    if st.session_state.new_todo != '':
+        todos.append(st.session_state.new_todo + '\n')
+        st.session_state['new_todo'] = ''
+        functions.write_todos(todos)
 
 st.title('My Todo App')
 
 st.subheader('This is my todo app.')
 st.write('This app is to increase your productivity.')
 
-todos = functions.get_todos()
+st.text_input(label='todo', label_visibility='hidden',
+              placeholder='Add new todo..',
+              on_change=update_new_todo(),
+              key='new_todo')
 
 for item in todos:
     st.checkbox(item)
-
-
-st.text_input(label='',
-              placeholder='Add new todo..',
-              on_change=add_todo(),
-              key='new_todo')
