ff180fd33503e910da0e689a8d82c1c8ee4b0d1a
be1ia1 <belial.agula@gmail.com>
2023-04-01 15:45:07 +0300

    day2 app9

--- app9-email-webcam-detection/emailing.py
+++ app9-email-webcam-detection/emailing.py
@@ -1,2 +1,26 @@
-def send_email():
-    print('email was send')
+import smtplib
+from email.message import EmailMessage
+import imghdr
+
+SENDER = 'belial.ahula@gmail.com'
+PASSWORD = 'tumssajhetkvtsjw'
+
+def send_email(image):
+    print('start mail process')
+    email_message = EmailMessage()
+    email_message['Subject'] = 'New customer showed up!'
+    email_message.set_content('Hey, we just saw a new customer!')
+    with open(image, 'rb') as fo:
+        content = fo.read()
+    email_message.add_attachment(content, maintype='image', subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP('smtp.gmail.com', 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, SENDER, email_message.as_string())
+    gmail.quit()
+    print('end mail process')
+
+if __name__ == '__main__':
+    send_email('images/7.png')
--- app9-email-webcam-detection/main.py
+++ app9-email-webcam-detection/main.py
@@ -1,15 +1,32 @@
 import cv2
 import time
+import glob
+import os
+from threading import Thread
 from emailing import send_email
 
 
-video = cv2.VideoCapture(0)
+video = cv2.VideoCapture(2)
 time.sleep(1)
 first_frame = None
 status_list = []
+count = 1
+
+def clean_images():
+    print('start clean process')
+    images = glob.glob('images/*.png')
+    for image in images:
+        print(image)
+        os.remove(image)
+    print('end clean process')
+
+# clean_images()
+
+
 while True:
     status = 0
     check, frame = video.read()
+
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
     
@@ -27,10 +44,21 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0))
         if rectangle.any():
             status = 1
+            cv2.imwrite(f'images/{count}.png', frame)
+            count += 1
+            images = glob.glob('images/*')
+            index = len(images) // 2
+            image_with_object = images[index]
     status_list.append(status)
     status_list = status_list[-2:]
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        email_thread = Thread(target=send_email, args=(image_with_object,))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_images)
+        clean_thread.daemon = True
+
+        email_thread.start()
+        
     cv2.imshow('My video', frame)
 
     key = cv2.waitKey(1)
@@ -38,3 +66,5 @@ while True:
         break
 
 video.release()
+
+clean_thread.start()
